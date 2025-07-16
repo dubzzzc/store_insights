6973f2a4c46a3d4b31a7841034931490d81ec771
Christopher C Wilson <35072403+dubzzzc@users.noreply.github.com>
2025-07-16 09:45:18 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,12 +2,11 @@ from fastapi import FastAPI
 from app.auth import router as auth_router
 from app.insights import router as insights_router
 
+app = FastAPI(title="Store Insights API")
+
 @app.get("/")
 def root():
     return {"message": "Store Insights API is running"}
-
-
-app = FastAPI(title="Store Insights API")
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(insights_router, prefix="/insights", tags=["insights"])
